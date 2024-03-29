f1ce74edf771cd778d48039f280e3be55f08d8de
Yogesh Wavhal <yswavhal@gmail.com>
2024-03-29 16:44:56 +0530

    #added fluent validation for validating the movieforcreationrequest model

---
+++ Robin.Movies/Robin.Movies.Api/Validators/MovieValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Robin.Movies.Api.Models;
+
+namespace Robin.Movies.Api.Validators
+{
+    /// <summary>
+    /// Validator for movie response models
+    /// </summary>
+    public class MovieValidator : AbstractValidator<MovieForCreationRequest>
+    {
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        public MovieValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage($"Title can not be empty.");
+            RuleFor(x => x.Cast).NotNull().WithMessage("Cast can not be null");
+        }
+    }
+}
